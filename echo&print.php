68e41dafa0a2ac387c7b9006bb5e0369dc4750d5
Nurunnahar <nurunnahar9081@gmail.com>
2023-10-20 07:58:09 +0600

    echo & print

---
+++ echo&print.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Hello Bangladesh\n";
+echo ("Hello Bangladesh\n");
+echo "hello"," ", "Bangladesh\n";
+echo "<h1>Hello Bangladesh</h1>";
+
+
+
+
+print "Hello Bangladesh\n";
+print ("Hello Bangladesh\n");
+// print "hello"," ", "Bangladesh\n";  //this is not work with print
+print "<h1>Hello Bangladesh</h1>";
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo "Hello I am Nasrin.......<br>";
-
-$name = "Nasrin";
-echo $name;
